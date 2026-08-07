b5fd9716f74d301af0311123dbe759a61ba1ad42
RBilly <rothstein.billy@gmail.com>
2026-08-07 21:48:45 +0200

    Merge task #11: LP3: Add a farewell function
    Squash merge 11-lp3-add-a-farewell-function into main.

--- src/greet.js
+++ src/greet.js
@@ -2,3 +2,8 @@
 export function greet(name) {
   return `Hello, ${name}!`;
 }
+
+/** Say goodbye to someone by name. */
+export function farewell(name) {
+  return `Goodbye, ${name}!`;
+}
--- test/greet.test.js
+++ test/greet.test.js
@@ -1,8 +1,12 @@
 import assert from "node:assert/strict";
 import { test } from "node:test";
 
-import { greet } from "../src/greet.js";
+import { greet, farewell } from "../src/greet.js";
 
 test("greets by name", () => {
   assert.equal(greet("Ada"), "Hello, Ada!");
+});
+
+test("bids farewell by name", () => {
+  assert.equal(farewell("Ada"), "Goodbye, Ada!");
 });
